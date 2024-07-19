6c07d2ef88698b3113f0a4bc00c4055ebddd6f07
Anna Valerio <anna.j.neff@gmail.com>
2024-07-19 12:22:16 -0500

    Passing 12/13 tests

--- rover.js
+++ rover.js
@@ -9,13 +9,45 @@ class Rover {
    }
 
    receiveMessage(message) {
-      this.message = message
+      this.message = message;
+      let results = [];
+      for (let i = 0; i < message.commands.length; i++){
+         if (message.commands[i].commandType === "MODE_CHANGE"){
+            this.mode = message.commands[i].value;
+            let modeChangeComplete = {"completed":true};
+            results.push(modeChangeComplete);
+         }
+      }
+      for (let k = 0; k < message.commands.length; k++){
+         if (message.commands[k].commandType === "MOVE"){
+            let moveCommandComplete;
+            if (this.mode === "NORMAL"){
+               this.position = message.commands[k].value;
+               moveCommandComplete = {"completed":true};
+            } else {
+               moveCommandComplete = {"completed":false};
+            }
+            results.push(moveCommandComplete);
+         }
+      }
+
+      for (let j = 0; j < message.commands.length; j++){
+         if (message.commands[j].commandType === "STATUS_CHECK"){
+            let roverStatusComplete = {"completed":true, "roverStatus":{"mode":this.mode, "generatorWatts":this.generatorWatts, "position":this.position}};
+            results.push(roverStatusComplete);
+         }
+      }
+    
       let response = {
          "message": message.messageName,
-         "results": [1, 2]
-
-      };
+         "results": results
+            // message.commands
+         };
+      
+     
+      
       return response;
+
       //returns an OBJECT containing at least two properties: message, the messageName of the original message, and:
       //results: an array of results. Each element in the array is an object that corresponds to one Command in message.commands
    }
@@ -24,8 +56,10 @@ class Rover {
 module.exports = Rover;
 
 // rover.js test code:
-let command1 = new Command("Command01", ["Action1", "Action2"]);
-let command2 = new Command("Command02", ["Action3", "Action4"]);
+let command1 = new Command("MOVE", 1010);
+// let command3 = new Command("MODE_CHANGE", "LOW_POWER")
+let command4 = new Command("STATUS_CHECK");
 let testRover = new Rover(1);
-let testMessage4 = new Message("TestMessage4", [command1.value[0], command2.value[0]])
-console.log(testRover.receiveMessage(testMessage4));
+let testMessage4 = new Message("TestMessage4", [command1, command4])
+
+console.log(testRover.receiveMessage(testMessage4).results);
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -16,21 +16,50 @@ describe("Rover class", function() {
   });
 
   it("response returned by receiveMessage contains the name of the message", function() {
-    let testRover2 = new Rover(8)
-    let testCommand1 = new Command("type1", 567);
-    let testCommand2 = new Command("type2", 123);
+    let testRover2 = new Rover(4)
+    let testCommand1 = new Command("MOVE", 567);
+    let testCommand2 = new Command("MODE_CHANGE", "LOW_POWER");
     let sampleMessage = new Message("Testing123", [testCommand1, testCommand2]);
     expect(testRover2.receiveMessage(sampleMessage).message).toBe("Testing123");
   });
 
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
-    let testRover3 = new Rover(9)
+    let testRover3 = new Rover(5)
     let testCommand3 = new Command("type1", 567);
     let testCommand4 = new Command("type2", 123);
     let sampleMessage2 = new Message("Testing123", [testCommand3, testCommand4]);
-    // left off on line 32, fixing brackets etc.
-    expect(testRover3.receiveMessage(sampleMessage2.commands)[testCommand3.value[0],testCommand4.value[0]]).toBe([567, 123]);
-  })
-  // 7 tests here!
+    // passing with to StrictEqual, let's add functionality and retest
+    expect(testRover3.receiveMessage(sampleMessage2).results).toStrictEqual([testCommand3, testCommand4]);
+  });
+
+  it("responds correctly to the status check command", function() {
+    let testRover4 = new Rover(6)
+    let testCommand3 = new Command("STATUS_CHECK");
+    let sampleMessage2 = new Message("Testing Status Check", [testCommand3]);
+    expect(testRover4.receiveMessage(sampleMessage2).results).toStrictEqual([{completed: true, roverStatus:{mode: 'NORMAL', generatorWatts: 110, position: 6}}]);
+  });
+
+  it("responds correctly to the mode change command", function() {
+    let testRover5 = new Rover(7)
+    let testCommand5 = new Command("MODE_CHANGE", 'LOW_POWER');
+    let sampleMessage2 = new Message("Testing Mode Change", [testCommand5]);
+    expect(testRover5.receiveMessage(sampleMessage2).results[0]).toStrictEqual({ completed:true });
+  });
+
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+    let testRover6 = new Rover(8, 'LOW_POWER', 110);
+    let testCommand5 = new Command("MODE_CHANGE", 'LOW_POWER');
+    let testCommand6 = new Command("MOVE", 456);
+    let sampleMessage2 = new Message("Testing Move Command In Low Power Mode", [testCommand6, testCommand5]);
+    expect(testRover6.receiveMessage(sampleMessage2).results).toStrictEqual([{ completed: true }, { completed: false }]);
+  });
+
+  it("responds with the position for the move command", function() {
+    let testRover7 = new Rover(9);
+    let testCommand7 = new Command("MOVE", 678);
+    let sampleMessage2 = new Message("Testing Move Command In Normal Mode", [testCommand7]);
+    testRover7.receiveMessage(sampleMessage2);
+    expect(testRover7.position).toBe(678);
+  });
 
 });
